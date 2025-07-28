f30d052fc9c1aa464e6f034b1af0708316550592
Raymond Connell <ray@MacBook-Pro.local>
2025-07-28 17:09:32 -0400

    Remove temporary debugging .cpp files from project root
    - Remove check_constant.cpp and check_throttle_value.cpp
    - These were temporary files used for debugging throttle values
    - Clean up project structure by removing development artifacts

--- check_constant.cpp
+++
@@ -1,6 +0,0 @@
-#include "include/lpx_webcam_server.h"
-#include <iostream>
-int main() {
-    std::cout << "KEY_THROTTLE_MS = " << lpx::LPXDebugClient::KEY_THROTTLE_MS << std::endl;
-    return 0;
-}
